4c15ebb876623299afe24f69526c9e71c921cc9f
Kevin Cripe <kevincripe6@gmail.com>
2024-03-20 20:38:23 -0500

    Finished file

--- M02_lab_caseStudy_if_else_while.py
+++ M02_lab_caseStudy_if_else_while.py
@@ -5,11 +5,22 @@
 # Dean's List or the Honor Roll.
 
 import sys
-# save last name as variable
-lname = input("Please enter a last name or press ZZZ to quit ")
-# if block to detect quit response
-if lname == 'ZZZ' or 'zzz':
-    sys.exit()
-# save first name as variable
-fname = input("Please enter a first name ")
 
+while True:
+    # save last name as variable
+    lname = input("Please enter a last name or press ZZZ to quit ")
+    #
+    if lname == 'ZZZ':
+        break
+    # save first name as variable
+    fname = input("Please enter a first name ")
+    # save gpa as a float
+    gpa = float(input("Please enter a GPA "))
+
+    # finding if student made list's
+    if gpa >= float(3.5):
+        print("Student has made the Deans List")
+    elif gpa >= float(3.25):
+        print("Student has made the Honor Roll")
+    else:
+        print("This student did not meet requirement")
